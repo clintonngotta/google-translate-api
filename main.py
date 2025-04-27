c6257e6b8371baddb5f9e783e955cd91ad2c8eb4
Clinton Ngotta <clintonngotta@gmail.com>
2025-04-28 01:42:35 +0300

    setup error handler

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi import Request
-
+from fastapi import HTTPException
 
 app = FastAPI()
 @app.get("/")
@@ -9,12 +9,24 @@ def read_root():
     return "Welcome to the Google Translate API"
 
 
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail},
+    )
+@app.exception_handler(Exception)
+async def general_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Internal Server Error"},
+    )
+
 @app.post("/translate")
 async def translate_text(
     text: str,
     target_lang: str,
     request: Request
-):
-    
+):    
     translated_text = f"Translated '{text}' to {target_lang}"
     return JSONResponse(content={"translated_text": translated_text})
